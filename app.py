a75671c4318965fb950e6e48c7b441ac4a1a6511
Jemmy <jemmyperez28@gmail.com>
2021-08-02 17:23:07 -0500

    Agregado Documentacion /swagger

--- app.py
+++ app.py
@@ -2,9 +2,12 @@
 | app.py : Archivo de aplicacion principal 
 '''
 from flask import Flask
+from flask.helpers import send_from_directory
 from flask_migrate import Migrate
 from config.database import db , ma
 from blueprints.persona_bp import persona
+
+from flask_swagger_ui import get_swaggerui_blueprint
 
 #Inicializacion de Objetos principales
 app = Flask(__name__)
@@ -15,6 +18,26 @@ migrate = Migrate(app, db)
 
 #Registro de Blueprints
 app.register_blueprint(persona)
+
+
+
+SWAGGER_URL = '/swagger'
+API_URL = '/static/swagger.json'
+swaggerui_blueprint = get_swaggerui_blueprint(
+    SWAGGER_URL,
+    API_URL,
+    config= {
+        'app_name' : "Documentacion con Swagger"
+    }
+)
+
+app.register_blueprint(swaggerui_blueprint, url_prefix =SWAGGER_URL)
+
+@app.route('/static/<path:path>')
+def send_static(path):
+    return send_from_directory('static',path)
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
